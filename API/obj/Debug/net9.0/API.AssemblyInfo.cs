d6ec3cfe8fa8e39ec01b99f45b837c5a65ef3ce8
Skitgin <cornelconstantingin@gamil.com>
2026-03-09 20:56:55 +0200

    wigo

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6556f26db716e4c9dbbf83b5da3c5041501da240")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0279cce5595550b842ac0a986c85500a20fdb148")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
